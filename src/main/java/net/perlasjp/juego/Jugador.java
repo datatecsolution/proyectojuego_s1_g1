bbe3c7dd9ad3865d498ffd086e0584aec8ffea4f
Amara Acosta <acostaamara@gmail.com>
2025-08-20 08:33:36 -0600

    Jugador

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -4,4 +4,31 @@ public class Jugador {
     private String nombre;
     private int salud;
     private int fuerza;
+
+    public int atacar(){
+
+    }
+    public void recibirDanio(int danio){
+
+    }
+    public boolean estaVivo(){
+
+    }  
+    public Jugador (String nombre){
+
+    }
+    public String getNombre(){
+
+    }
+    public int getSalud(){
+
+    }
+    public void mostrarEstado(){
+
+    }
+
 }
+
+
+
+
