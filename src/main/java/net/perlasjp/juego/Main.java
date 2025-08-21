3d0f139e09f0da47f6701389cadb1580743b9c0a
ashleygamer08 <ashleyyesseniaflores@gmail.com>
2025-08-21 15:33:46 -0600

    Ashley2

--- src/main/java/net/perlasjp/juego/Main.java
+++ src/main/java/net/perlasjp/juego/Main.java
@@ -1,15 +1,8 @@
 package net.perlasjp.juego;
  
 public class Main {
-<<<<<<< HEAD
     public static void main(String[] args){
 
-        
-    }
-=======
-  
-    public static void main(String[] args) {
 
-  }
->>>>>>> ebb1995c869ce41f2d1a1c163c343f09a366bc99
+    }
 }
