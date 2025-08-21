3ae10c48ce6faff82b7d798f38ce35f9e55028d8
Yoensi <Yoensirodriguez0@gmail.com>
2025-08-21 09:02:08 -0600

    Enemigo

--- src/main/java/net/perlasjp/juego/Enemigo.java
+++ src/main/java/net/perlasjp/juego/Enemigo.java
@@ -4,20 +4,24 @@ public class Enemigo {
    private String nombre;
    private int salud;
    private int fuerza;
-}
+
 
 public int atacar(){
-
+  return 0;
 }
 public void recibirDanio(int danio){
 
 }
 public boolean estavivo(){
+   return false;
 
 }
  public String getNombre(){
+   return nombre;
 
  }
  public int getSalud(){
+   return 0;
         
+ }
  }
