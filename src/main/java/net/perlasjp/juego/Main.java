455db56fe79d53cf22644c973cccb53b6bbf9477
Ashley <ashleyyesseniaflores@gmail.com>
2025-08-27 21:22:29 -0600

    Limpiar Main.java: eliminar línea de comentario y dejar método main vacío

--- src/main/java/net/perlasjp/juego/Main.java
+++ src/main/java/net/perlasjp/juego/Main.java
@@ -1,6 +1,7 @@
 package net.perlasjp.juego;
-    public class Main {
-        public static void main(String[] args) {
-    
+
+public class Main {
+    public static void main(String[] args) {
+        
     }
 }
