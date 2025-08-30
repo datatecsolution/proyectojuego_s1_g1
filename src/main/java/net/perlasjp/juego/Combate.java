439e7410112370b5038825047b0f2e3648a3ff85
Kendy Ruiz <ruizk9992@gmail.com>
2025-08-29 20:39:16 -0600

    Combate

--- src/main/java/net/perlasjp/juego/Combate.java
+++ src/main/java/net/perlasjp/juego/Combate.java
@@ -5,6 +5,28 @@ public class Combate{
     private Enemigo enemigo;
 
     public void iniciar(Jugador jugador,Enemigo enemigo){
-        
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\niUn " + enemigo.getNombre() + " ha aparecido!");
+            while (jugador.estaVivo() && enemigo.estaVivo()){
+                System.out.println("\nTu salud " + jugador.getSalud() + " | Salud del enemigo: " +
+    enemigo.getSalud());
+                System.out.print("¿Atacar (a) o Huir (h)? ");
+                String opcion = scanner.nextLine();
+
+                if (opcion.equalsIgnoreCase("a")) {
+                    jugador.recibirDanio(enemigo.atacar());
+                } else {
+                    System.out.println("Derrotaste al " + enemigo.getNombre() + "!");
+                }
+            } else if(opcion.equalsIgnoreCase("h")) {
+                System.out.println("Has huido del combate.");
+                return;
+            } else {
+                System.out.println("opcion no valida.");
+            }
+         }
+         if (!jugador.estaVivo()) {
+            System.out.println("Has muerto en combate. Fin del juego.");
+            System.exit(0);
+         }
     }
-}
